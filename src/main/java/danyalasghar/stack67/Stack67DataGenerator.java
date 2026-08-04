66cc4643ff344b2826a6638448f22897084447fb
danify21 <danyalthebest74@gmail.com>
2026-08-04 00:39:57 -0400

    Initial commit

---
+++ src/main/java/danyalasghar/stack67/Stack67DataGenerator.java
@@ -0,0 +1,11 @@
+package danyalasghar.stack67;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Stack67DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
